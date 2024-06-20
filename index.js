bffaa962576503354539d34546492a76ba1865ea
Pearl Nerijean Calape <pearlnerijeancalape@gmail.com>
2024-06-20 17:34:05 +0000

    2nd try

--- index.js
+++ index.js
@@ -12,7 +12,13 @@ app.get('/', function (req, res) {
     res.sendFile(process.cwd() + '/views/index.html');
 });
 
-app.post('/api/fileanalyse', multer().single('upfile'), (req, res) => {
+const upload = multer();
+
+app.post('/api/fileanalyse', upload.single('upfile'), (req, res) => {
+    if (!req.file) {
+        return res.status(400).json({ error: 'No file uploaded' });
+    }
+
     res.json({
         name: req.file.originalname,
         type: req.file.mimetype,
